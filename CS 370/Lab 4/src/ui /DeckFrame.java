bfa52df322700c63109250a66f8056a8175d2d35
love24-code <106122738+love24-code@users.noreply.github.com>
2026-03-06 17:32:44 -0800

    Create DeckFrame.java

---
+++ CS 370/Lab 4/src/ui /DeckFrame.java
@@ -0,0 +1,27 @@
+package ui;
+
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class DeckFrame extends JFrame {
+    private DeckPanel deckPanel;
+    private JButton shuffleButton;
+
+    public DeckFrame(int width, int height) {
+        super("Random Line Chart");
+
+        setLayout(new BorderLayout());
+
+        deckPanel = new DeckPanel();
+        shuffleButton = new JButton("Shuffle");
+
+        shuffleButton.addActionListener(e -> deckPanel.shuffleAndRedrawDeck());
+
+        add(deckPanel, BorderLayout.CENTER);
+        add(shuffleButton, BorderLayout.SOUTH);
+
+        setSize(width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
