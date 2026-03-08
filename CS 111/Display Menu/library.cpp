5a35148efb3df63ca161ae6787e01ae340f8aa67
love24-code <106122738+love24-code@users.noreply.github.com>
2026-03-07 16:41:50 -0800

    Add library.cpp to display menu options

---
+++ CS 111/Display Menu/library.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "library.h"
+
+using namespace std;
+
+void displayMenu() {
+    cout << "Menu:\n";
+    cout << "1. Option 1\n";
+    cout << "2. Option 2\n";
+    cout << "3. Option 3\n";
+    cout << "4. Exit\n";
+}
+
+void option1() {
+    cout << "Option 1 selected.\n";
+}
+
+void option2() {
+    cout << "Option 2 selected.\n";
+}
+
+void option3() {
+    cout << "Option 3 selected.\n";
+}
